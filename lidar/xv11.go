63a1f46189e70286ec1482ff7b8dc8e254614a37
Jason Harris <jasonh@mistsys.com>
2016-08-09 08:09:44 -0700

    More LIDAR code.

--- lidar/xv11.go
+++ lidar/xv11.go
@@ -30,6 +30,77 @@ type LIDAR struct {
 }
 
 //-----------------------------------------------------------------------------
+// PID Parameters for Motor Speed Control
+
+const LIDAR_RPM = 300.0        // target value
+const LIDAR_DEFAULT_PWM = 0.20 // initial setting
+
+const LIDAR_PID_KP = 0.0
+const LIDAR_PID_TI = 0.0
+const LIDAR_PID_TD = 0.0
+const LIDAR_PID_DT = 0.0
+
+//-----------------------------------------------------------------------------
+// LIDAR Frame
+
+const LIDAR_SOF_DELIMITER = 0xfa
+const LIDAR_MIN_INDEX = 0xa0
+const LIDAR_MAX_INDEX = 0xf9
+const SAMPLES_PER_FRAME = 4
+
+type LIDAR_frame struct {
+	index    uint8  // 0xa0 - 0xf9 (0-89 offset, 4 samples per frame = 360 measurements)
+	start    uint8  // 0xfa
+	speed    uint16 // little endian, rpm = speed/64
+	samples  [SAMPLES_PER_FRAME]uint32
+	checksum uint16
+}
+
+//-----------------------------------------------------------------------------
+// LIDAR Sample
+
+type LIDAR_sample struct {
+	no_data   bool   // No return/max range/too low of reflectivity
+	too_close bool   // Object too close, possible poor reading due to proximity < 0.6m
+	dist      uint16 // distance
+	ss        uint16 // ??
+}
+
+// extract information from a frame sample
+func (frame *LIDAR_frame) sample(i int) *LIDAR_sample {
+
+	x := frame.samples[i]
+	b0 := (x >> 0) & 0xff
+	b1 := (x >> 8) & 0xff
+	b2 := (x >> 16) & 0xff
+	b3 := (x >> 24) & 0xff
+
+	var sample LIDAR_sample
+	sample.no_data = (b0>>7)&1 != 0
+	sample.too_close = (b0>>6)&1 != 0
+	sample.dist = uint16(((b0 & 0x3F) << 8) + b1)
+	sample.ss = uint16((b2 << 8) + b3)
+	return &sample
+}
+
+//-----------------------------------------------------------------------------
+
+// endian flip
+func flip16(data uint16) uint16 {
+	return (data >> 8) | (data << 8)
+}
+
+// return the checksum for a LIDAR data frame
+func calc_checksum(data [10]uint16) uint16 {
+	var cs uint32
+	for i := 0; i < 10; i++ {
+		cs = (cs << 1) + uint32(flip16(data[i]))
+	}
+	cs = ((cs & 0x7fff) + (cs >> 15)) & 0x7fff
+	return uint16(cs)
+}
+
+//-----------------------------------------------------------------------------
 
 func (lidar *LIDAR) stop() {
 	log.Printf("lidar.stop() %s\n", lidar.Name)
