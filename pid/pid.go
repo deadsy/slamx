4d979722075f1a594a0e8cf3749e6b4a88cb8764
Jason Harris <sirmanlypowers@gmail.com>
2020-09-19 11:48:03 -0700

    Cleanup of PID package.

--- pid/pid.go
+++ pid/pid.go
@@ -1,14 +1,7 @@
-//-----------------------------------------------------------------------------
-/*
-
-Generic PID Control
-
-References:
-https://en.wikipedia.org/wiki/PID_controller
-
-*/
-//-----------------------------------------------------------------------------
-
+// Package pid provides a generic PID Controller.
+//
+// References:
+// https://en.wikipedia.org/wiki/PID_controller
 package pid
 
 import (
@@ -18,59 +11,60 @@ import (
 
 //-----------------------------------------------------------------------------
 
+// PID controller state.
 type PID struct {
-	kp      float32 // proportional constant
-	ki      float32 // integral constant
-	kd      float32 // derivative constant
-	i_max   float32 // max limit on i_term
-	i_min   float32 // min limit on i_term
-	o_max   float32 // max limit on output
-	o_min   float32 // min limit on output
-	sp      float32 // set point value (target)
-	ev_prev float32 // previous error value
-	i_term  float32 // integral sum
-	d_flag  bool    // avoid spiking d_term on the first update
+	kp     float32 // proportional constant
+	ki     float32 // integral constant
+	kd     float32 // derivative constant
+	iMax   float32 // max limit on iTerm
+	iMin   float32 // min limit on iTerm
+	oMax   float32 // max limit on output
+	oMin   float32 // min limit on output
+	sp     float32 // set point value (target)
+	evPrev float32 // previous error value
+	iTerm  float32 // integral sum
+	dFlag  bool    // avoid spiking the derivative term on the first update
 }
 
 //-----------------------------------------------------------------------------
 
-// Update the PID, return the control value
-func (pid *PID) Update(pv float32) float32 {
+// Update the PID, return the control value.
+func (p *PID) Update(pv float32) float32 {
 
-	ev := pid.sp - pv
+	ev := p.sp - pv
 
 	// proportional
-	p_term := pid.kp * ev
+	pTerm := p.kp * ev
 
 	// integral
-	if pid.ki != 0.0 {
-		pid.i_term += pid.ki * ev
+	if p.ki != 0.0 {
+		p.iTerm += p.ki * ev
 		// limit the integration sum
-		if pid.i_term > pid.i_max {
-			pid.i_term = pid.i_max
+		if p.iTerm > p.iMax {
+			p.iTerm = p.iMax
 		}
-		if pid.i_term < pid.i_min {
-			pid.i_term = pid.i_min
+		if p.iTerm < p.iMin {
+			p.iTerm = p.iMin
 		}
 	}
 
 	// derivative
-	if !pid.d_flag {
-		// avoid spiking the d_term on the first update
-		pid.ev_prev = ev
-		pid.d_flag = true
+	if !p.dFlag {
+		// avoid spiking the dTerm on the first update
+		p.evPrev = ev
+		p.dFlag = true
 	}
-	d_term := pid.kd * (ev - pid.ev_prev)
-	pid.ev_prev = ev
+	dTerm := p.kd * (ev - p.evPrev)
+	p.evPrev = ev
 
 	// calculate and limit the output
-	out := p_term + pid.i_term + d_term
-	if out > pid.o_max {
-		out = pid.o_max
+	out := pTerm + p.iTerm + dTerm
+	if out > p.oMax {
+		out = p.oMax
 		log.Printf("limiting max pid output %f", out)
 	}
-	if out < pid.o_min {
-		out = pid.o_min
+	if out < p.oMin {
+		out = p.oMin
 		log.Printf("limiting min pid output %f", out)
 	}
 
@@ -84,41 +78,41 @@ func (p *PID) Set(sp float32) {
 	p.sp = sp
 }
 
-// Reset the PID state variables.
+// Reset the PID controller state.
 func (p *PID) Reset() {
 	p.sp = 0
-	p.ev_prev = 0
-	p.i_term = 0
-	p.d_flag = false
+	p.evPrev = 0
+	p.iTerm = 0
+	p.dFlag = false
 }
 
 //-----------------------------------------------------------------------------
 
-// Initialise the PID state
-func Init(dt, kp, ki, kd, i_min, i_max, o_min, o_max float32) (*PID, error) {
+// Init initialises the PID controller state.
+func Init(dt, kp, ki, kd, iMin, iMax, oMin, oMax float32) (*PID, error) {
 
-	var pid PID
+	var p PID
 
-	if dt < 0.0 || kp < 0.0 || ki < 0.0 || kd < 0.0 || i_min > i_max || o_min > o_max {
+	if dt < 0.0 || kp < 0.0 || ki < 0.0 || kd < 0.0 || iMin > iMax || oMin > oMax {
 		return nil, errors.New("invalid PID parameters")
 	}
 
-	pid.kp = kp
-	pid.ki = ki * dt
-	pid.kd = kd / dt
+	p.kp = kp
+	p.ki = ki * dt
+	p.kd = kd / dt
 
-	pid.i_min = i_min
-	pid.i_max = i_max
+	p.iMin = iMin
+	p.iMax = iMax
 
-	pid.o_min = o_min
-	pid.o_max = o_max
+	p.oMin = oMin
+	p.oMax = oMax
 
-	pid.sp = 0.0
-	pid.ev_prev = 0.0
-	pid.i_term = 0.0
-	pid.d_flag = false
+	p.sp = 0
+	p.evPrev = 0
+	p.iTerm = 0
+	p.dFlag = false
 
-	return &pid, nil
+	return &p, nil
 }
 
 //-----------------------------------------------------------------------------
